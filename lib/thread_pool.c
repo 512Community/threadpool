ff70f687c70a0147113b376b0412353df6b0ba4d
Qin TianLong <qintl@yytek.com>
2018-11-06 17:13:02 +0800

    调整代码，添加了pthread族函数返回值判断，控制线程函数化
    Signed-off-by: Qin TianLong <qintl@yytek.com>

--- lib/thread_pool.c
+++ lib/thread_pool.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <errno.h>
 #include <unistd.h>
 #include <signal.h>
+#include <errno.h>
 #include <msg_log.h>
 #include <thread_pool.h>
 
+#ifndef true
 #define true 1
+#endif
+#ifndef false
 #define false 0
+#endif
 
+#define PTHREAD_ERR(func, loop) do { \
+	if(func) { 	\
+		ESLOG_ERR(#func " failed:%d\n", errno);\
+		goto loop; 		\
+	}  				\
+} while(0)\
 
 /*任务*/
 struct threadpool_task {
@@ -60,6 +70,8 @@ static void *threadpool_thread(void *threadpool);
 static int threadpool_free(struct threadpool **__pool);
 static void *admin_thread(void *threadpool);
 static int is_thread_alive(pthread_t tid);
+static int thread_create(struct threadpool *threadpool);
+static int thread_destory(struct threadpool *threadpool);
 
 int threadpool_create(void **__pool,
 		      unsigned int default_thr_num,
@@ -106,20 +118,15 @@ int threadpool_create(void **__pool,
 		goto out;
 	}
 
-	if (pthread_mutex_init(&(pool->mutex), NULL) || pthread_mutex_init(&(pool->busy_thr_num_mutex), NULL)) {
-		ESLOG_ERR("pthread mutex init false, errno:%d\n", errno);
-		goto out;
-	} 
-
-	if (pthread_cond_init(&(pool->queue_not_empty), NULL) || pthread_cond_init(&(pool->queue_not_full), NULL)) {
-		ESLOG_ERR("pthread cond init false, errno:%d\n", errno);
-		goto out;
-	}
+	PTHREAD_ERR(pthread_mutex_init(&(pool->mutex), NULL), out);
+	PTHREAD_ERR(pthread_mutex_init(&(pool->busy_thr_num_mutex), NULL), out);
+	PTHREAD_ERR(pthread_cond_init(&(pool->queue_not_empty), NULL), out);
+	PTHREAD_ERR(pthread_cond_init(&(pool->queue_not_full), NULL), out);
 
 	for (i = 0; i < min_thr_num; i++)
-		pthread_create(&(pool->threads[i]), NULL, threadpool_thread, (void *)pool);
+		PTHREAD_ERR(pthread_create(&(pool->threads[i]), NULL, threadpool_thread, (void *)pool), out);
 
-	pthread_create(&pool->admin_tid, NULL, admin_thread, (void *)pool);
+	PTHREAD_ERR(pthread_create(&pool->admin_tid, NULL, admin_thread, (void *)pool), out);
 
 	*__pool = pool;
 
@@ -145,16 +152,18 @@ int threadpool_destroy(void *_pool, int flags)
 
 	pool->shutdown = true;
 
-	pthread_join(pool->admin_tid, NULL);
+	PTHREAD_ERR(pthread_join(pool->admin_tid, NULL), out);
 
 	for (i = 0; i < POOL(pool, thread, live_num); i++)
-		pthread_cond_broadcast(&(pool->queue_not_empty));
+		PTHREAD_ERR(pthread_cond_broadcast(&(pool->queue_not_empty)), out);
 
 	for (i = 0; i < POOL(pool, thread, live_num); i++)
-		pthread_join(pool->threads[i], NULL);
+		PTHREAD_ERR(pthread_join(pool->threads[i], NULL), out);
 
 	threadpool_free(&pool);
 	return 0;
+out:
+	return -1;
 }
 
 int threadpool_add_task(void *_pool, void *(*function)(void *arg), void *arg)
@@ -165,7 +174,7 @@ int threadpool_add_task(void *_pool, void *(*function)(void *arg), void *arg)
 	unsigned int queue_max_size;
 	struct threadpool *pool = _pool;
 
-	pthread_mutex_lock(&(pool->mutex));
+	pthread_mutex_lock(&pool->mutex);
 
 	queue_front = POOL(pool, queue, front);
 	queue_rear = POOL(pool, queue, rear);
@@ -174,10 +183,10 @@ int threadpool_add_task(void *_pool, void *(*function)(void *arg), void *arg)
 
 	/*如果队列满了,调用wait阻塞*/
 	while ((POOL(pool, queue, size) == queue_max_size) && (!pool->shutdown))
-		pthread_cond_wait(&(pool->queue_not_full), &(pool->mutex));
+		PTHREAD_ERR(pthread_cond_wait(&pool->queue_not_full, &pool->mutex), out);
 
 	if (pool->shutdown) {
-		pthread_mutex_unlock(&(pool->mutex));
+		PTHREAD_ERR(pthread_mutex_unlock(&pool->mutex), out);
 		return -1;
 	}
 
@@ -195,10 +204,13 @@ int threadpool_add_task(void *_pool, void *(*function)(void *arg), void *arg)
 	POOL(pool, queue, size)++;
 
 	/*添加完任务后,队列就不为空了,唤醒线程池中的一个线程*/
-	pthread_cond_signal(&(pool->queue_not_empty));
-	pthread_mutex_unlock(&(pool->mutex));
+	PTHREAD_ERR(pthread_cond_signal(&pool->queue_not_empty), out);
+	pthread_mutex_unlock(&pool->mutex);
 
 	return 0;
+out:
+	pthread_mutex_unlock(&pool->mutex);
+	return -1;
 }
 
 
@@ -213,10 +225,10 @@ static int threadpool_free(struct threadpool **__pool)
 	if (pool->threads)
 		free(pool->threads);
 
-	pthread_mutex_destroy(&(pool->mutex));
-	pthread_mutex_destroy(&(pool->busy_thr_num_mutex));
-	pthread_cond_destroy(&(pool->queue_not_empty));
-	pthread_cond_destroy(&(pool->queue_not_full));
+	pthread_mutex_destroy(&pool->mutex);
+	pthread_mutex_destroy(&pool->busy_thr_num_mutex);
+	pthread_cond_destroy(&pool->queue_not_empty);
+	pthread_cond_destroy(&pool->queue_not_full);
 
 	if (pool->task_queue)
 		free(pool->task_queue);
@@ -230,75 +242,90 @@ static int threadpool_free(struct threadpool **__pool)
 /*管理线程*/
 static void *admin_thread(void *threadpool)
 {
-	int i, add;
-	unsigned int queue_size;
-	unsigned int default_thr_num;
-	unsigned int live_thr_num;
-	unsigned int max_thr_num;
-	unsigned int busy_thr_num;
 
 	struct threadpool *pool = threadpool;
-
-	pthread_mutex_lock(&(pool->mutex));
-	max_thr_num = POOL(pool, thread, max_num);
-	default_thr_num = POOL(pool, thread, default_num);
-	pthread_mutex_unlock(&(pool->mutex));
-
 	while (!pool->shutdown) {
-		pthread_mutex_lock(&(pool->mutex));
-		queue_size = POOL(pool, queue, size);
-		live_thr_num = POOL(pool, thread, live_num);
-		pthread_mutex_unlock(&(pool->mutex));
+		if(thread_create(pool) < 0)
+			return NULL;
 
-		pthread_mutex_lock(&(pool->busy_thr_num_mutex));
-		busy_thr_num = POOL(pool, thread, busy_num);
-		pthread_mutex_unlock(&(pool->busy_thr_num_mutex));
-
-		/* 创建新线程--live的线程数小于实际任务数量,并且存活线程数小于最大线程数 */
-		/* 如果实际任务很多,并且执行的速度很慢，那么线程池很快就会到达max */
-		if (live_thr_num < queue_size && live_thr_num < max_thr_num) {
-			printf("live_thr_num:%d\n",live_thr_num);
-			pthread_mutex_lock(&(pool->mutex));
-
-			/* 如果 pool->threads中一直没有有效值,则循环结束,主要防止max小于default的情况发生 */
-			/* 如果 创建default个线程，则循环结束 */
-			for (i = 0, add = 0; i < max_thr_num && add < default_thr_num; i++) {
-				/* 如果live此时大于max，那么就不在继续创建 */
-				if (POOL(pool, thread, live_num) > max_thr_num)
-					break;
-
-				if (pool->threads[i] == 0 || !is_thread_alive(pool->threads[i])) {
-					if (pthread_create(&(pool->threads[i]), NULL, threadpool_thread, (void *)pool)) {
-						ESLOG_ERR("pthread create false\n");
-						pthread_mutex_unlock(&(pool->mutex));
-						return NULL;
-					}
-					add++;
-					POOL(pool, thread, live_num)++;
-				}
-			}
-			pthread_mutex_unlock(&(pool->mutex));
-		}
-
-		/* busy的线程x2 大于live的线程，并且live的线程大于min线程,通知线程池的线程进行自杀 */
-		/* busy x2并没有明确的要求 */
-		/* live > min 是为了保证线程池最少有min个线程living */
-		if ((busy_thr_num * 2) < live_thr_num  &&  live_thr_num > POOL(pool, thread, min_num)) {
-			ESLOG_INFO("admin busy:%d live:%d----\n", busy_thr_num, live_thr_num);
-
-			pthread_mutex_lock(&(pool->mutex));
-			POOL(pool, thread, exit_num) = default_thr_num;
-			pthread_mutex_unlock(&(pool->mutex));
-
-			for (i = 0; i < default_thr_num; i++) {
-				//通知正在处于空闲的线程，自杀
-				pthread_cond_signal(&(pool->queue_not_empty));
-				ESLOG_INFO("admin cler --\n");
-			}
-		}
+		if(thread_destory(pool) < 0)
+			return NULL;
 	}
 
 	return NULL;
+}
+static int thread_create(struct threadpool *pool)
+{
+	unsigned int i, add, queue_size;
+	unsigned int default_thr_num, live_thr_num, max_thr_num;
+
+	pthread_mutex_lock(&pool->mutex);
+	max_thr_num = POOL(pool, thread, max_num);
+	default_thr_num = POOL(pool, thread, default_num);
+	queue_size = POOL(pool, queue, size);
+	live_thr_num = POOL(pool, thread, live_num);
+	pthread_mutex_unlock(&pool->mutex);
+
+	/* 创建新线程--live的线程数小于实际任务数量,并且存活线程数小于最大线程数 */
+	/* 如果实际任务很多,并且执行的速度很慢，那么线程池很快就会到达max */
+	if (live_thr_num < queue_size && live_thr_num < max_thr_num) {
+		pthread_mutex_lock(&pool->mutex);
+
+		/* 如果 pool->threads中一直没有有效值,则循环结束,主要防止max小于default的情况发生 */
+		/* 如果 创建default个线程，则循环结束 */
+		for (i = 0, add = 0; i < max_thr_num && add < default_thr_num; i++) {
+			/* 如果live此时大于max，那么就不在继续创建 */
+			if (POOL(pool, thread, live_num) > max_thr_num)
+				break;
+
+			if (pool->threads[i] == 0 || !is_thread_alive(pool->threads[i])) {
+				PTHREAD_ERR(pthread_create(&(pool->threads[i]), NULL, threadpool_thread, (void *)pool), out);
+				add++;
+				POOL(pool, thread, live_num)++;
+			}
+		}
+		pthread_mutex_unlock(&pool->mutex);
+	}
+
+	return 0;
+out:
+	pthread_mutex_unlock(&pool->mutex);
+	return -1;
+
+}
+
+static int thread_destory(struct threadpool *pool)
+{
+	unsigned int i, queue_size, default_thr_num;
+	unsigned int live_thr_num, min_thr_num, busy_thr_num;
+
+	pthread_mutex_lock(&pool->mutex);
+	min_thr_num = POOL(pool, thread, min_num);
+	default_thr_num = POOL(pool, thread, default_num);
+	queue_size = POOL(pool, queue, size);
+	live_thr_num = POOL(pool, thread, live_num);
+	pthread_mutex_unlock(&pool->mutex);
+
+	pthread_mutex_lock(&pool->busy_thr_num_mutex);
+	busy_thr_num = POOL(pool, thread, busy_num);
+	pthread_mutex_unlock(&pool->busy_thr_num_mutex);
+	/* busy的线程x2 大于live的线程，并且live的线程大于min线程,通知线程池的线程进行自杀 */
+	/* busy x2并没有明确的要求 */
+	/* live > min 是为了保证线程池最少有min个线程living */
+	if ((busy_thr_num * 2) < live_thr_num  &&  live_thr_num > min_thr_num) {
+		pthread_mutex_lock(&pool->mutex);
+		POOL(pool, thread, exit_num) = default_thr_num;
+		pthread_mutex_unlock(&pool->mutex);
+
+		for (i = 0; i < default_thr_num; i++) {
+			//通知正在处于空闲的线程，自杀
+			PTHREAD_ERR(pthread_cond_signal(&pool->queue_not_empty), out);
+		}
+	}
+
+	return 0;
+out:
+	return -1;
 }
 
 static int is_thread_alive(pthread_t tid)
@@ -320,29 +347,24 @@ static void *threadpool_thread(void *threadpool)
 		pthread_mutex_lock(&pool->mutex);
 		//无任务等待，有任务则跳出
 		while (!(POOL(pool, queue, size) | pool->shutdown)) {
-			ESLOG_INFO("thread 0x%x is waiting \n", (unsigned int)pthread_self());
-			pthread_cond_wait(&(pool->queue_not_empty), &(pool->mutex));
+			PTHREAD_ERR(pthread_cond_wait(&pool->queue_not_empty, &pool->mutex), out);
 
 			//判断是否需要清除线程,自杀功能
 			if (POOL(pool, thread, exit_num) == 0)
 				continue;
 
-			//ESLOG_INFO("exit_num:%d\n", POOL(pool, thread, exit_num));
-			printf("exit_num:%d\n", POOL(pool, thread, exit_num));
+			ESLOG_INFO("exit_num:%d\n", POOL(pool, thread, exit_num));
 			POOL(pool, thread, exit_num)--;
 			//如果live的线程数大于min线程，则销毁掉.
 			if (POOL(pool, thread, live_num) > POOL(pool, thread, min_num)) {
-				ESLOG_INFO("thread 0x%x is exiting \n", (unsigned int)pthread_self());
 				POOL(pool, thread, live_num)--;
 				goto out;
 			}
 		}
 
 		//线程池开关状态
-		if (pool->shutdown) {
-			ESLOG_INFO("shutdown true 0x%x \n", (unsigned int)pthread_self());
+		if (pool->shutdown)
 			goto out;
-		}
 
 		//否则该线程可以拿出任务
 		task.function = pool->task_queue[POOL(pool, queue, front)].function;
@@ -353,13 +375,12 @@ static void *threadpool_thread(void *threadpool)
 
 
 		//通知可以添加新任务
-		pthread_cond_broadcast(&pool->queue_not_full);
+		PTHREAD_ERR(pthread_cond_broadcast(&pool->queue_not_full), out);
 
 		//释放线程锁
 		pthread_mutex_unlock(&pool->mutex);
 
 		//执行刚才取出的任务
-		ESLOG_INFO("thread 0x%x start working \n", (unsigned int)pthread_self());
 		pthread_mutex_lock(&pool->busy_thr_num_mutex);
 		POOL(pool, thread, busy_num)++;
 		pthread_mutex_unlock(&pool->busy_thr_num_mutex);
@@ -367,14 +388,12 @@ static void *threadpool_thread(void *threadpool)
 		(*(task.function))(task.arg);
 
 		//任务结束处理
-		ESLOG_INFO("thread 0x%x end working \n", (unsigned int)pthread_self());
 		pthread_mutex_lock(&pool->busy_thr_num_mutex);
 		POOL(pool, thread, busy_num)--;
 		pthread_mutex_unlock(&pool->busy_thr_num_mutex);
 	}
 out:
-
-	pthread_mutex_unlock(&(pool->mutex));
+	pthread_mutex_unlock(&pool->mutex);
 
 	return NULL;
 }
